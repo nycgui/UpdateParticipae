9404b97c4285ddfa90d9a08a3c225dd7b8d025a5
FabinMatts <fabio.a.mattos@hotmail.com>
2020-06-17 12:12:51 -0300

    Crawler alterado: Dados em uma variavel string

--- CrawlerPrefeitura/crawler.js
+++ CrawlerPrefeitura/crawler.js
@@ -1,7 +1,7 @@
 const request = require('request');
 var fs = require('fs');
 const options = {
-  //este codigo ira ate o link e irá pegar as informações  necessarias para o webcrawler usando o metodo GET
+
   url: 'http://www.licitacao.pmmc.com.br/Transparencia/vencimentos2',
 
   method: 'GET',
@@ -14,15 +14,20 @@ const options = {
 };
 
 request(options,function (err, res, body){
+  
+  //a linha abaixo joga as informações do body para a variavel json
+  let json = JSON.parse(body)
 
-  let json = JSON.parse(body);
-  //o codigo abaixo cria um arquivo .json com as informações do site organizando ela e exibindo uma mensagem de sucesso
-  fs.writeFile('servidores.json', JSON.stringify(json, null, 4), function(err){
+  //a linha abaixo transforma o conteudo da variavel json em string e lança na variavel dados.
+  let dados=JSON.stringify(json,null,4)
 
-    console.log('Arquivo criado com sucesso. Verifique a pasta.');
-
-})
-  //o comando abaixo exibiria o mesmo conteudo do arquivo servidores.json no log 
-  //console.log(json);
-
+  //a linha abaixo serve apenas para exibir o conteudo da variavel dados para testar o funcionamento.
+  console.log(dados)
+  
+  /*
+  decidi usar duas variaveis para ser facil de entender. A variavel 'json' contem o conteudo
+  nao convertido do body do site, ja a variavel 'dados' contem o conteudo do body convertido em uma string.
+  Para poder ver se tem diferença, quando coloca a variavel json para ser exibida com console.log, ela nao mostrara todo o 
+  conteudo do body, ja a variavel dados exibira tudo.
+  */
 });
