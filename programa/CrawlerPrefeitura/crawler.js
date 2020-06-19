bd0636334c58fdb16f4a9e4fb236389f6fedc49f
FabinMatts <fabio.a.mattos@hotmail.com>
2020-06-19 09:08:01 -0300

    Crawler Editado para facilitar a busca de elementos do array

--- programa/CrawlerPrefeitura/crawler.js
+++ programa/CrawlerPrefeitura/crawler.js
@@ -20,14 +20,20 @@ request(options,function (err, res, body){
 
   //a linha abaixo transforma o conteudo da variavel json em string e lança na variavel dados.
   let dados=JSON.stringify(json,null,4)
-
-  //a linha abaixo serve apenas para exibir o conteudo da variavel dados para testar o funcionamento.
-  console.log(dados)
+  //as linhas a seguir removem espaços indesejados da array deixando apenas as informações desejadas
+  var espaço = /:\s|,\s/
+  var separar = dados.split(espaço)
+  //um teste para verificar as informações do array
+  fs.writeFileSync('Servidores.json' , separar)
+  //um teste para pegar um ponto especifico do array
+  console.log(separar[0])
   
   /*
-  decidi usar duas variaveis para ser facil de entender. A variavel 'json' contem o conteudo
-  nao convertido do body do site, ja a variavel 'dados' contem o conteudo do body convertido em uma string.
+  decidi usar três variaveis para ser facil de entender. A variavel 'json' contem o conteudo
+  nao convertido do body do site, ja a variavel 'dados' contem o conteudo do body convertido em uma string e a variavel separar divide o array
+  de forma que facilite puxar informações desejadas.
   Para poder ver se tem diferença, quando coloca a variavel json para ser exibida com console.log, ela nao mostrara todo o 
-  conteudo do body, ja a variavel dados exibira tudo.
+  conteudo do body, ja a variavel dados exibira tudo, e a variavel separar faz com que o array tenha apenas as informações relevantes para 
+  o sistema.
   */
 });
